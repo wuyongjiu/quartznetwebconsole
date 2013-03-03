ebc76d6808dcaa4234c72947035ec67b92296bb1
ValeraKolupaev <ValeraKolupaev@localhost>
2013-03-03 09:55:20 -0500

    default cron jobs

--- QuartzNet.HostApplication/Program.cs
+++ QuartzNet.HostApplication/Program.cs
@@ -28,17 +28,21 @@ namespace QuartzNet.HostApplication
             var sched = schedFact.GetScheduler();
             sched.Start();
 
-            for (var i = 1; i < 10; i++)
-            {
-                var job = JobBuilder.Create().OfType<HelloJob>().WithIdentity("myJob-" + i).Build();
-                var trigger =
-                    TriggerBuilder.Create()
-                                  .ForJob(job)
-                                  .WithDailyTimeIntervalSchedule(s => s.OnEveryDay().WithIntervalInMinutes(i*5))
-                                  .Build();
-                sched.ScheduleJob(job, trigger);
-            }
+            CreateJob(sched, "10-13-Wen-Fri-Job", (TriggerBuilder t) => t.WithCronSchedule  ("0 30 10-13 ? * WED,FRI"));
+            CreateJob(sched, "8-10-5th-20th-Job", (TriggerBuilder t) => t.WithCronSchedule  ("0 0/30 8-9 5,20 * ?"));
+            CreateJob(sched, "11-16-Job", (TriggerBuilder t) => t.WithCronSchedule          ("0 0 11,16 * * ?"));
+            CreateJob(sched, "9-18-Mon-Fri-Job", (TriggerBuilder t) => t.WithCronSchedule   ("0 0 09-18 ? * 1-5"));
+
             return schedFact;
+        }
+
+        private static void CreateJob(IScheduler sched, string name, Func<TriggerBuilder, TriggerBuilder> triggerBuilder)
+        {
+            var job = JobBuilder.Create().OfType<HelloJob>().WithIdentity(name).Build();
+            var trigger =
+                TriggerBuilder.Create()
+                              .ForJob(job);
+            sched.ScheduleJob(job, triggerBuilder(trigger).Build());
         }
     }
 
