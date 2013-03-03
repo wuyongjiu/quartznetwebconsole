819749219757504d8eeaac4b7d3134adce6d5c5f
ValeraKolupaev <ValeraKolupaev@localhost>
2013-03-03 14:37:25 -0500

    ACL list

---
+++ QuartzNet.WebConsole/AccessControl/AclListBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuartzNet.WebConsole.AccessControl
+{
+    public class AclListBuilder
+    {
+        private readonly List<AclRule> _rules = new List<AclRule>();
+        public void Add(Func<string, bool?> user = null, Func<string, bool?> action = null,
+                        Func<string, bool?> job = null)
+        {
+            user = user ?? (s => true);
+            action = action ?? (s => true);
+            job = job ?? (s => true);
+
+            _rules.Add(new AclRule(user, action, job));
+        }
+
+        public bool Verify(string user, string action, string job)
+        {
+            var result = _rules.Select(r => r.Verify(user, action, job)).FirstOrDefault(r => r.HasValue);
+            return result ?? false;
+        }
+    }
+}
---
+++ QuartzNet.WebConsole/AccessControl/AclRule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace QuartzNet.WebConsole.AccessControl
+{
+    public class AclRule
+    {
+        private readonly Func<string, bool?> _user;
+        private readonly Func<string, bool?> _action;
+        private readonly Func<string, bool?> _job;
+
+        public AclRule(Func<string, bool?> user, Func<string, bool?> action, Func<string, bool?> job)
+        {
+            _user = user;
+            _action = action;
+            _job = job;
+        }
+
+        public bool? Verify(string user, string action, string job)
+        {
+            return _user(user) & _action(action) & _job(job);
+        }
+    }
+}
