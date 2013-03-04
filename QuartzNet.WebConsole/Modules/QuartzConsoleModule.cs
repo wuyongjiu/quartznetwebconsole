ff8dccfe401dcf538b7725426fea9076259a6749
ValeraKolupaev <ValeraKolupaev@localhost>
2013-03-03 21:14:16 -0500

    navigation - highlight for active page

--- QuartzNet.WebConsole/Modules/QuartzConsoleModule.cs
+++ QuartzNet.WebConsole/Modules/QuartzConsoleModule.cs
@@ -25,10 +25,12 @@ namespace QuartzNet.WebConsole.Modules
             After.AddItemToEndOfPipeline(ctx =>
                 {
                     ViewBag.machineName = Environment.MachineName;
-                    ViewBag.timezones = System.TimeZoneInfo.GetSystemTimeZones().Select(q => Tuple.Create(q.Id, q.DisplayName)).ToArray();
+                    ViewBag.timezones =
+                        TimeZoneInfo.GetSystemTimeZones().Select(q => Tuple.Create(q.Id, q.DisplayName)).ToArray();
                     ViewBag.selectedZone = _timeZoneService.GetUserTimeZone().Id;
                 });
-            Get[""] = (p) =>
+            Get["/"] = (p) => Response.AsRedirect("~/quartzconsole/index");
+            Get["/index"] = (p) =>
                 {
                     var m = new JobListViewModel
                         {
@@ -53,9 +55,9 @@ namespace QuartzNet.WebConsole.Modules
                 };
             Post["/trigger/{schedKey}/{jobKey}"] = p =>
                 {
-                    var scheduler = schedFact.GetScheduler((string)p.schedKey);
+                    var scheduler = schedFact.GetScheduler((string) p.schedKey);
                     JobKey key = null;
-                    var keyStr = ((string)p.jobKey);
+                    var keyStr = ((string) p.jobKey);
                     if (keyStr.Contains("."))
                     {
                         var keyArr = keyStr.Split('.');
@@ -67,47 +69,46 @@ namespace QuartzNet.WebConsole.Modules
                     }
 
                     scheduler.TriggerJob(key);
-                    return Response.AsRedirect("/quartzconsole");
+                    return Response.AsRedirect("~/quartzconsole/index");
                 };
             Get["/schedule"] = p =>
                 {
                     var jobs = (
-                        from sched in schedFact.AllSchedulers
-                        from jobGroup in sched.GetJobGroupNames()
-                        from jobKey in sched.GetJobKeys(GroupMatcher<JobKey>.GroupContains(jobGroup))
-                        select jobKey.ToString()
-                        ).ToList();
+                                   from sched in schedFact.AllSchedulers
+                                   from jobGroup in sched.GetJobGroupNames()
+                                   from jobKey in sched.GetJobKeys(GroupMatcher<JobKey>.GroupContains(jobGroup))
+                                   select jobKey.ToString()
+                               ).ToList();
 
-                    return View["Schedule", new { jobs }];
+                    return View["Schedule", new {jobs}];
                 };
             Post["/ScheduleJson"] = p =>
                 {
-                    var start = _timeZoneService.FromUser(FromUnixTimestamp((long)Request.Form.start));
-                    var end = _timeZoneService.FromUser(FromUnixTimestamp((long)Request.Form.end));
-                    var activeJobKeys = ((string)Request.Form["activeJobs[]"] ?? "").Split(',');
+                    var start = _timeZoneService.FromUser(FromUnixTimestamp((long) Request.Form.start));
+                    var end = _timeZoneService.FromUser(FromUnixTimestamp((long) Request.Form.end));
+                    var activeJobKeys = ((string) Request.Form["activeJobs[]"] ?? "").Split(',');
                     var triggers = (
                                        from sched in schedFact.AllSchedulers
                                        from triggerGroup in sched.GetTriggerGroupNames()
-                                       from triggerKey in sched.GetTriggerKeys(GroupMatcher<TriggerKey>.GroupContains(triggerGroup))
+                                       from triggerKey in
+                                           sched.GetTriggerKeys(GroupMatcher<TriggerKey>.GroupContains(triggerGroup))
                                        let trig = sched.GetTrigger(triggerKey)
                                        join ajk in activeJobKeys on trig.JobKey.ToString() equals ajk
                                        select trig
-                                       ).ToList();
+                                   ).ToList();
                     var lst = new List<object>();
                     lst.AddRange(triggers.SelectMany(trigger => GetAllTimes(trigger, start, end).Select(t => new
-                    {
-                        start = _timeZoneService.ToUser(t).ToString("yyyy-MM-dd hh:mm:ss"),
-                        title = trigger.JobKey.ToString(),
-                        allDay = false
-                    })));
+                        {
+                            start = _timeZoneService.ToUser(t).ToString("yyyy-MM-dd hh:mm:ss"),
+                            title = trigger.JobKey.ToString(),
+                            allDay = false
+                        })));
 
                     return Response.AsJson(lst);
                 };
-            Post["/setTimezone"] = p =>
-                {
-                    return Response.AsRedirect(Request.Headers.Referrer).AddCookie(_timeZoneService.GetCookieFor((string)Request.Form.timezone));
-                };
-
+            Post["/setTimezone"] =
+                p => Response.AsRedirect(Request.Headers.Referrer)
+                             .AddCookie(_timeZoneService.GetCookieFor((string) Request.Form.timezone));
         }
 
         private IEnumerable<DateTimeOffset> GetAllTimes(ITrigger trigger, DateTimeOffset startTime, DateTimeOffset end)
@@ -121,14 +122,12 @@ namespace QuartzNet.WebConsole.Modules
                     break;
                 yield return time.Value;
             }
-
         }
 
-        DateTime FromUnixTimestamp(long timestamp)
+        private DateTime FromUnixTimestamp(long timestamp)
         {
             return new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(timestamp);
         }
-
 
 
         private JobViewModel CreateJobInfo(IJobDetail job, IScheduler scheduler, ISet<JobKey> runningJobs)
--- QuartzNet.WebConsole/Views/HtmlHelpers.cs
+++ QuartzNet.WebConsole/Views/HtmlHelpers.cs
@@ -1,6 +1,8 @@
 ﻿#region
 
+using Nancy;
 using Nancy.Helpers;
+using Nancy.ViewEngines.Razor;
 
 #endregion
 
@@ -16,6 +18,13 @@ namespace QuartzNet.WebConsole.Views
         public static string HtmlEncode(this string str)
         {
             return HttpUtility.HtmlEncode(str);
+        }
+
+        public static string IsActive<TModel>(this UrlHelpers<TModel> urlHelpers, string url, string activeClass)
+        {
+            if (urlHelpers.Content(url) == urlHelpers.RenderContext.Context.Request.Url.Path)
+                return activeClass;
+            return string.Empty;
         }
     }
 }
